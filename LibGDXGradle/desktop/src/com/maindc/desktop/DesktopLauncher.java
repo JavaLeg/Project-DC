303a85ad920cd98884e7237f2790da90f83e7641
JavaLeg <jamesz96.au@gmail.com>
2017-09-18 17:53:16 +1000

    Set up LibGDX gradle

---
+++ LibGDXGradle/desktop/src/com/maindc/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.maindc.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.maindc.DC_Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DC_Main(), config);
+	}
+}
---
+++ LibGDXGradle/html/src/com/maindc/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.maindc.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.maindc.DC_Main;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new DC_Main();
+        }
+}
