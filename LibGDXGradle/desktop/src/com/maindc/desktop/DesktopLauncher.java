941d65c04b93cafa8d68ab13a9822ae0888fb3e7
JavaLeg <jamesz96.au@gmail.com>
2017-09-20 22:31:31 +1000

    Skeleton Interface added

--- LibGDXGradle/desktop/src/com/maindc/desktop/DesktopLauncher.java
+++ LibGDXGradle/desktop/src/com/maindc/desktop/DesktopLauncher.java
@@ -2,11 +2,19 @@ package com.maindc.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.engine.desktop.MyGame;
 import com.maindc.DC_Main;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new DC_Main(), config);
+		
+		
+	      config.width = 800;
+	      config.height = 480;
+	      config.title = "Project-DC";
+		
+	      
+	      new LwjglApplication(new MyGame(), config);
 	}
 }
