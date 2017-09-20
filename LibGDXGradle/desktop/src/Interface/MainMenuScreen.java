05cb257093857a2d17aebeeb6b1709efbc0bc265
JavaLeg <jamesz96.au@gmail.com>
2017-09-20 22:33:01 +1000

    Interface package added

---
+++ LibGDXGradle/desktop/src/Interface/MainMenuScreen.java
@@ -0,0 +1,135 @@
+package Interface;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.engine.desktop.MyGame;
+
+public class MainMenuScreen implements Screen {
+
+    protected Stage stage;
+    private Viewport viewport;
+    private OrthographicCamera camera;
+    private TextureAtlas atlas;
+    protected Skin skin;
+    final MyGame game;
+    
+    private static final int WORLD_WIDTH  = 250;
+    private static final int WORLD_HEIGHT = 250;
+	
+    public MainMenuScreen(final MyGame game)
+    {
+    	this.game = game;
+    	
+        atlas = new TextureAtlas(Gdx.files.internal("uiskin.atlas"));
+        skin = new Skin(Gdx.files.internal("uiskin.json"), atlas);
+
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(WORLD_HEIGHT, WORLD_WIDTH, camera);
+        viewport.apply();
+
+        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
+        camera.update();
+
+        stage = new Stage(viewport);    
+    }
+	
+	@Override
+	public void show() {
+		//Stage should control input:
+        Gdx.input.setInputProcessor(stage);
+
+        //Create Table
+        Table mainTable = new Table();
+        //Set table to fill stage
+        mainTable.setFillParent(true);
+        //Set alignment of contents in the table.
+        mainTable.top();
+
+        //Create buttons
+        TextButton playButton = new TextButton("Play", skin);
+        TextButton optionsButton = new TextButton("Options", skin);
+        TextButton exitButton = new TextButton("Exit", skin);
+
+        //Add listeners to buttons
+        
+        playButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                ((Game)Gdx.app.getApplicationListener()).setScreen(new PlayScreen());
+            }
+        });
+        exitButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit();
+            }
+        });
+
+        //Add buttons to table
+        mainTable.add(playButton);
+        mainTable.row();
+        mainTable.add(optionsButton);
+        mainTable.row();
+        mainTable.add(exitButton);
+        
+        //Add table to stage
+        stage.addActor(mainTable);
+		
+	}
+
+	@Override
+	public void render(float delta) {
+        Gdx.gl.glClearColor(.1f, .12f, .16f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.act();
+        stage.draw();
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+        viewport.update(width, height);
+        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
+        camera.update();
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+        skin.dispose();
+        atlas.dispose();
+		
+	}
+	
+}
---
+++ LibGDXGradle/desktop/src/com/engine/desktop/MyGame.java
@@ -0,0 +1,32 @@
+package com.engine.desktop;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import Interface.MainMenuScreen;
+
+public class MyGame extends Game{
+
+	public SpriteBatch batch;
+	public BitmapFont font;
+	
+	@Override
+	public void create() {
+		batch = new SpriteBatch();
+		//Use LibGDX's default Arial font.
+		font = new BitmapFont();
+		this.setScreen(new MainMenuScreen(this));
+	}
+	
+	public void render() {
+		super.render();
+	}
+	
+	public void dispose() {
+		batch.dispose();
+		font.dispose();
+	}
+}
+
+
