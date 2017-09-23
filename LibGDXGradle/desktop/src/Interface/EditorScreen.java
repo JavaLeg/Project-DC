42b0e56b704cabbd384ae36b30cde14e434c0b4e
MatthewC221 <matthewchen42@gmail.com>
2017-09-23 17:21:55 +1000

    Clicklistener implemented

--- LibGDXGradle/desktop/src/Interface/EditorScreen.java
+++ LibGDXGradle/desktop/src/Interface/EditorScreen.java
@@ -1,23 +1,24 @@
 package Interface;
 
-import java.util.ArrayList;
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Array;
 
 public class EditorScreen implements Screen {
 	
     private SpriteBatch batch;
     private Texture texture;
-    private Array <Sprite> sprites;
+    private Array <Sprite> sprites;		// The grids
 	
-    private int height;
+    // Dimensions of screen
+    private int height;			
     private int width;
+    public Vector3 touchPos;			// The click spot
  
 
 	// Show only operates once, after it will render
@@ -27,8 +28,10 @@ public class EditorScreen implements Screen {
 		width = Gdx.graphics.getWidth();
 		sprites = new Array<Sprite>();
         batch = new SpriteBatch();
+        touchPos = new Vector3();
         Texture cur_texture = new Texture("tmp.jpg");
         
+        // Create grid system and put into sprites to be drawn
         for (int j = 0; j < height; j = j + 50) {
 	        for (int i = 0; i < width; i = i + 50) {
 	            Sprite cur_sprite = new Sprite(cur_texture);
@@ -36,9 +39,9 @@ public class EditorScreen implements Screen {
 	            cur_sprite.setCenter(i, j);
 	            sprites.add(cur_sprite);
 	        }
-        }
-	        
+        } 
 	}
+
 	
 	@Override
 	public void render(float delta) {
@@ -46,9 +49,15 @@ public class EditorScreen implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         
         batch.begin();
+        // Draw sprites and listen for inputs.
 		for (Sprite cur_sprite : sprites) {
         	cur_sprite.draw(batch);
         }
+		if (Gdx.input.isTouched()) {
+			// Insert into touchPos to calculate later?
+			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0); 
+			System.out.println("X = " + touchPos.x + ", Y = " + touchPos.y);
+		}
         batch.end();
 	}
 
