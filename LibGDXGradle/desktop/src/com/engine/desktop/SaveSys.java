c0f5999bb3bacd8e771b8b98f84ddcd993643c41
JavaLeg <jamesz96.au@gmail.com>
2017-09-24 09:15:03 +1000

    Serialization of objects added under SaveSys.

---
+++ LibGDXGradle/desktop/src/com/engine/desktop/SaveSys.java
@@ -0,0 +1,33 @@
+package com.engine.desktop;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+/*
+ * Save System
+ */
+
+public class SaveSys {
+
+	/*
+	 * Object -> byte[]
+	 */
+    public byte[] Save(DCGame g) throws IOException{
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        ObjectOutputStream os = new ObjectOutputStream(out);
+        os.writeObject(g);
+        return out.toByteArray();    
+    }
+    
+	/*
+	 * byte[] -> Object
+	 */
+    public DCGame Load(byte[] data) throws IOException, ClassNotFoundException {
+        ByteArrayInputStream in = new ByteArrayInputStream(data);
+        ObjectInputStream is = new ObjectInputStream(in);
+        return (DCGame)is.readObject();
+    }
+}
