9eeac64e75346b9eda26aa72b54675880f89fb5e
MatthewC221 <matthewchen42@gmail.com>
2017-09-23 14:25:51 +1000

    Drawing sprites, was trying to play with tiledMaps but not worth it...

--- LibGDXGradle/desktop/src/Interface/EditorScreen.java
+++ LibGDXGradle/desktop/src/Interface/EditorScreen.java
@@ -1,63 +1,39 @@
 package Interface;
 
+import java.util.ArrayList;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Camera;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Array;
 
 public class EditorScreen implements Screen {
 	
-	private OrthographicCamera cam;
-	public ShapeRenderer shapeRenderer;
-	private ClickListener click;
+    private SpriteBatch batch;
+    private Texture texture;
+    private Sprite sprite;
 	
+	public Array <Sprite> sprites;
+
 	// Show only operates once, after it will render
 	@Override
 	public void show() {
-		
-		// Get height, width of current screen 
-		int height = Gdx.graphics.getHeight();
-		int width = Gdx.graphics.getWidth();
-		
-		click = new ClickListener();
-		cam = new OrthographicCamera(height, width);
-		shapeRenderer = new ShapeRenderer();
-		
-		Gdx.gl.glClearColor(0, 0, 0, 0);
-		Gdx.gl.glClear( GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT );	
-
-		cam.update();
-		shapeRenderer.setProjectionMatrix(cam.combined);
-		shapeRenderer.begin(ShapeType.Line);
-		shapeRenderer.setColor(Color.WHITE);
-		
-		// Draw the vertical lines
-		for (int i = 0; i < width; i = i + 10) {
-			shapeRenderer.line(i, 0, i, height);
-		}
-		
-		// Draw horizontal lines
-		for (int i = 0; i < height; i = i + 10) {
-			shapeRenderer.line(0, i, width, i);
-		}
-
-		shapeRenderer.end();
+        batch = new SpriteBatch();
+        texture = new Texture("temp_tile.jpg");
+        sprite = new Sprite(texture);
 	}
 	
 	@Override
 	public void render(float delta) {
-		// Constantly render when grids are clicked 
-		
-		// Doesn't currently work
-		if (click.isPressed()) {
-			System.out.println("Click!");
-		}
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        
+        batch.begin();
+        sprite.draw(batch);
+        batch.end();
 	}
 
 	@Override
@@ -86,6 +62,8 @@ public class EditorScreen implements Screen {
 
 	@Override
 	public void dispose() {
+        batch.dispose();
+        texture.dispose();
 		// TODO Auto-generated method stub
 		
 	}
