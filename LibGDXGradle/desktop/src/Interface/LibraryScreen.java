a037ad8db38f3b839d6bcd94d05ac038bd658244
JavaLeg <jamesz96.au@gmail.com>
2017-09-24 22:55:37 +1000

    Added new LibScreen class

--- LibGDXGradle/desktop/src/Interface/LibraryScreen.java
+++ LibGDXGradle/desktop/src/Interface/LibraryScreen.java
@@ -1,5 +1,91 @@
 package Interface;
 
-public class LibraryScreen {
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+public class LibraryScreen implements Screen{
+	
+    protected Stage stage;
+    private Viewport viewport;
+    private OrthographicCamera camera;
+    private TextureAtlas atlas;
+    protected Skin skin;
+    
+    private static final int WORLD_WIDTH  = 250;
+    private static final int WORLD_HEIGHT = 250;
+
+	@Override
+	public void show() {
+        atlas = new TextureAtlas(Gdx.files.internal("uiskin.atlas"));
+        skin = new Skin(Gdx.files.internal("uiskin.json"), atlas);
+
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(WORLD_HEIGHT, WORLD_WIDTH, camera);
+        viewport.apply();
+
+        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
+        camera.update();
+
+        stage = new Stage(viewport);  
+        
+		//Stage should control input:
+        Gdx.input.setInputProcessor(stage);
+				
+	}
+
+	@Override
+	public void render(float delta) {
+        Gdx.gl.glClearColor(.1f, .12f, .16f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.act();
+        stage.draw();
+		
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+        viewport.update(width, height);
+        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
+        camera.update();
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
