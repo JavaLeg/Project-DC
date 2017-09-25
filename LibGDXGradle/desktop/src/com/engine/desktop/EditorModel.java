f1c3b63e0172f9490ccfa091c81832aaefc3ff2e
JavaLeg <jamesz96.au@gmail.com>
2017-09-25 19:13:33 +1000

    Editor model Core progress

--- LibGDXGradle/desktop/src/com/engine/desktop/EditorModel.java
+++ LibGDXGradle/desktop/src/com/engine/desktop/EditorModel.java
@@ -1,5 +1,7 @@
 package com.engine.desktop;
 
+import java.io.IOException;
+
 import com.badlogic.gdx.maps.tiled.TiledMap;
 
 public class EditorModel {
@@ -8,31 +10,62 @@ public class EditorModel {
 	private Tile[][] map;
 	private Tile selectedTile;
 	private int selected[];
+	private SaveSys saveHandle;
 	
-	public EditorModel() {
+	public EditorModel() throws IOException {
 		this.m = new TiledMap();
 		this.state = new State();
 		this.saveHandle = new SaveSys();
 		
 	}
 	
+	/*
+	 * Any click on the preview screen in EditorScreen should call this function
+	 */
 	public void select(int row, int col) {
 		selected[0] = row;
 		selected[1] = col;
 	}
 	
-	public void addWall() {
-		state.setTerrain(selected, TERRAIN.WALL);
-	}
-	
+	public void setTerrain(TERRAIN t) {
+		
+		switch(t) {
+		case WALL:
+			state.setWall(selected);
+			break;
+		case TRAP:
+			state.setTrap(selected);
+			break;
+		}
 
-	
-	public void addItem() {
-		state.setTerrain(selected, GAME_OBJ.ITEM);
+
 	}
 	
+	public void setGameObj(GAME_OBJ g) {
+		switch(g) {
+		
+		// Item should be its own thing??
+		// Can add an enum later
+		case ITEM:
+			state.setItem(selected);
+			break;
+		case ENEMY:
+			state.setEnemy(selected);
+			break;
+		case PLAYER:
+			state.setPlayer(selected);
+		}
+	}
+
+	/*
+	 * Should clear the tile regardless of what terrain/item
+	 */
 	public void clearTile() {
-		state.deleteTile();
+		state.deleteTile(selected);
+	}
+	
+	public void saveMap(String filename) {
+		saveHandle.Save(state, filename);
 	}
 	
 	
