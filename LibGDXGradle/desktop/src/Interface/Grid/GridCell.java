adbe490a42a6aa012bd21fd7201e3c3579782b91
JavaLeg <jamesz96.au@gmail.com>
2017-10-10 14:21:37 +1100

    Sprite family extracted from tilesets grid changes

--- LibGDXGradle/desktop/src/Interface/Grid/GridCell.java
+++ LibGDXGradle/desktop/src/Interface/Grid/GridCell.java
@@ -1,14 +1,34 @@
 package Interface.Grid;
 
-public class GridCell {
-	private static final gridWidth = 40;
-	private static final gridHeight = 40;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+
+public class GridCell extends Image{
+	private static final gridWidth;
+	private static final gridHeight;
 	
 	public final int xPos;
 	public final int yPos;
 	
+	private Image currImage;
+	
 	public GridCell(int x, int y) {
 		this.xPos = x;
 		this.yPos = y;
+		
+		this.gridWidth = 40;
+		this.gridHeight = 40;
+		
+		// Determine grid lines
+		int left = xPos * gridWidth;
+		int right = left + gridWidth;
+		int bottom = yPos * gridHeight;
+		int top = bottom + gridHeight;
+		
+		// Set default
+		Image currImage = new Image(new TextureRegion(new Texture(
+				Gdx.files.internal("empty.png"))));
+		
 	}
+	
 }
