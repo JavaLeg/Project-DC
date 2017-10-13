5cbad8a1b7ce395f1faf5c74d75ed8f41d1ed239
Oscar Duretto <Oscar Duretto@10.248.131.103>
2017-10-13 14:42:12 +1100

    BLARGH

--- LibGDXGradle/desktop/src/Tileset/EnemySubclass/EnemySlime.java
+++ LibGDXGradle/desktop/src/Tileset/EnemySubclass/EnemySlime.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 import State.Coordinates;
 import State.MovePathToPoint;
+import State.MoveRandom;
 import Tileset.Enemy;
 import Tileset.GameObject;
 
@@ -13,7 +14,7 @@ public class EnemySlime extends Enemy {
 	private static Texture texture = GameObject.getTexture(imageName, 0, 144, 16, 16).getTexture();
 	
 	public EnemySlime(Coordinates coords) {
-		super(30, 10, coords, texture, 50, 10, 30, new MovePathToPoint()); // width, height, coords, hp, damage, moveRate, moveBehaviour
+		super(30, 10, coords, texture, 50, 10, 30, new MoveRandom(ObjectType.ENEMY)); // width, height, coords, hp, damage, moveRate, moveBehaviour
 		
 	}
 }
