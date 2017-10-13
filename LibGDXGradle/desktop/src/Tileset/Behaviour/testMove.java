29d1db3d557c5f6e3a8262c8c54706ed1a4c3277
Oscar Duretto <Oscar Duretto@10.248.131.103>
2017-10-13 16:27:31 +1100

    Hiiii

--- LibGDXGradle/desktop/src/Tileset/Behaviour/testMove.java
+++ LibGDXGradle/desktop/src/Tileset/Behaviour/testMove.java
@@ -7,18 +7,21 @@ import State.State;
 
 public class testMove {
 	public static void main (String[] arg) {
-		MoveBehaviour m = new MoveBehaviour();
-		State s = new State(20, 20);
-		List<Coord> p = m.findRoute(s, new Coord(0,0), new Coord(6,8));
-		if (p == null) System.out.print("No path found.\n");
-		else {
-			System.out.print("Found path:\n");
-			for (Coord c : p) {
-				System.out.print(c.toString() + "\n");
-			}
-			
-		}
-	
+		//MoveBehaviour m = new MoveBehaviour();
+		//State s = new State(20, 20);
+		//List<Coord> p = m.findRoute(s, new Coord(0,0), new Coord(6,8));
+		//if (p == null) System.out.print("No path found.\n");
+		//else {
+		//	System.out.print("Found path:\n");
+		//	for (Coord c : p) {
+		//		System.out.print(c.toString() + "\n");
+		//	}
+		//}
+		//Gson gson = new Gson();
+		
+		//String jsonInString = "{'x': 1, 'y': 2}";
+		//Coord staff = gson.fromJson(jsonInString, Coord.class);
+		
 		
 	}
 }
