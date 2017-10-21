dc46ac2050952fd2d9b867a2b7bbc4af3c4d103b
JavaLeg <jamesz96.au@gmail.com>
2017-10-21 23:58:09 +1100

    Resizing tester

--- LibGDXGradle/desktop/src/com/maindc/desktop/DesktopLauncher.java
+++ LibGDXGradle/desktop/src/com/maindc/desktop/DesktopLauncher.java
@@ -3,7 +3,6 @@ package com.maindc.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.engine.desktop.DCGame;
-import com.maindc.DC_Main;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
