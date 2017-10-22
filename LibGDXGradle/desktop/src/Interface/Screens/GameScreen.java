4b26a4aa49641139218f0a7adb42071056f8d543
JavaLeg <jamesz96.au@gmail.com>
2017-10-22 21:57:02 +1100

    screen

--- LibGDXGradle/desktop/src/Interface/Screens/GameScreen.java
+++ LibGDXGradle/desktop/src/Interface/Screens/GameScreen.java
@@ -1,5 +1,6 @@
 package Interface.Screens;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
@@ -124,8 +125,8 @@ public class GameScreen implements Screen {
         	int width = (int)(play.getHp() / play.getMaxHp() * 40);
         	green_bar.setWidth(width);
         } else if (play != null && play.getHp() <= 0) {
-        	EndScreen screen = new EndScreen(game);
-        	screen.show();
+        	((Game)Gdx.app.getApplicationListener()).setScreen(new EndScreen(game));
+        	this.dispose();
         }
         
         
