82da429b80a16233b80c5a62121798d4619c47b2
JavaLeg <jamesz96.au@gmail.com>
2017-10-22 13:57:25 +1100

    Merge fixes

--- LibGDXGradle/desktop/src/Interface/Stages/Toolbar.java
+++ LibGDXGradle/desktop/src/Interface/Stages/Toolbar.java
@@ -39,21 +39,11 @@ public class Toolbar extends Stage{
 	public Toolbar(Viewport v, Skin skin, EditorScreen es) {
 		super(v);
 		this.skin = skin;
-<<<<<<< HEAD
+
 		System.out.println("Viewport width: " + v.getScreenWidth());
 		System.out.println("Viewport height: " + v.getScreenHeight());
 		this.toolbarPos = new TableTuple(v.getScreenWidth()*3/7, v.getScreenHeight()/46);
-		this.es = es;
-=======
-
-//		System.out.println("Viewport width: " + v.getScreenWidth());
-//		System.out.println("Viewport height: " + v.getScreenHeight());
-//		this.toolbarPos = new TableTuple(v.getScreenWidth()*3/7, v.getScreenHeight()/47);
-
-		this.toolbarPos = new TableTuple(v.getScreenWidth()*2/23, v.getScreenHeight()/45);
->>>>>>> refs/remotes/origin/master
-		
-		
+		this.es = es;		
 		initialise();
 	}
 	
